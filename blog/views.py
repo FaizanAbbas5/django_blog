2858f3d449d4b2b583a773c5f836478f66c305fc
Faizan Abbas <faizankhuwaja5@gmail.com>
2023-12-25 21:43:46 +0000

    :sparkels: Added templates and views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+posts = [
+    {
+        "author": "Faizi Baloch",
+        "title": "My first post",
+        "content": "Content for my first post",
+        "date_posted": "25th December"
+    },
+    {
+        "author": "Faizan",
+        "title": "My Second Post",
+        "content": "Content for my second post",
+        "date_posted": "25 December"
+    }
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html')
