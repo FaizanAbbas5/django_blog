b062d06a5ce0d7d7df63b952bdb9166681642eb1
Faizan Abbas <faizankhuwaja5@gmail.com>
2023-12-30 17:34:58 +0000

    :card_file_box: Added model 'Post'

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+# A table in the database
+class Post(models.Model):
+    # fields in the table
+    title = models.CharField(max_length = 100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default = timezone.now)
+    # foreign key (the user model already exists)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
 # data to be passed in the template
-posts = [
-    
-    # each dictionary contains information related to a post
-    {
-        "author": "Faizi Baloch",
-        "title": "My first post",
-        "content": "Content for my first post",
-        "date_posted": "25th December"
-    },
-    {
-        "author": "Faizan",
-        "title": "My Second Post",
-        "content": "Content for my second post",
-        "date_posted": "25 December"
-    }
-]
 
 def home(request):
     # we put our data into a dictionary
     # the key here is what is going to be accessible in our templates
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     # we pass context as the third argument
     return render(request, 'blog/home.html', context)
